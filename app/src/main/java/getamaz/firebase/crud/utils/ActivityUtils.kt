74abaffeddcb05578e258f3d9ce903a7a8f3be1e
Husnain <husnainalirafique125@gmail.com>
2023-09-25 22:34:07 +0500

    Changed updateNote code

--- app/src/main/java/getamaz/firebase/crud/utils/ActivityUtils.kt
+++ app/src/main/java/getamaz/firebase/crud/utils/ActivityUtils.kt
@@ -1,26 +1,15 @@
 package getamaz.firebase.crud.utils
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
 import android.view.View
 import android.view.WindowInsetsController
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModel
 
 fun Activity.startActivity(clazz: Class<*>) {
     val intent = Intent(this, clazz)
-    startActivity(intent)
-}
-
-fun Activity.startActivity(clazz: Class<*>, bundle: Bundle) {
-    val intent = Intent(this, clazz)
-    intent.putExtras(bundle)
     startActivity(intent)
 }
 
@@ -37,13 +26,7 @@ fun Activity.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun FragmentActivity.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, text, duration).show()
-}
 
-fun AppCompatActivity.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, text, duration).show()
-}
 fun setStatusBarAppearance(view: View) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         val insetsController = view.windowInsetsController
