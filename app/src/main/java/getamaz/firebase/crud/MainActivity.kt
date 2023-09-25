aea3ccd629a17cac38eccc190abde99fcfd3bb09
Husnain <husnainalirafique125@gmail.com>
2023-09-25 13:13:53 +0500

    CRUD

---
+++ app/src/main/java/getamaz/firebase/crud/MainActivity.kt
@@ -0,0 +1,11 @@
+package getamaz.firebase.crud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
